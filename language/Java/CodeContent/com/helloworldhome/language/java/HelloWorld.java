fc8e9e43da30df764793d7bd0a2116bb262a01e1
yanchangyou <yanchangyou@gmail.com>
2014-09-07 20:17:32 +0800

    提交java实现

---
+++ language/Java/CodeContent/com/helloworldhome/language/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.helloworldhome.language.java;
+
+/**
+ * 第一个java程序：hello, world
+ * 
+ * @author yanchangyou@gmail.com
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello, world");
+	}
+}
